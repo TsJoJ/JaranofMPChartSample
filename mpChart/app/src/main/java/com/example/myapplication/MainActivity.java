07b32875f96d8e4aee79a12aa9e03bbb11e041a0
TsJoJ <josef@basiricogianmarco.it>
2020-05-29 17:20:27 +0200

    francesca esplode computer

--- mpChart/app/src/main/java/com/example/myapplication/MainActivity.java
+++ mpChart/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ciao andrea che ci fai qui
+        //francesca esplode computer
     }
 }
